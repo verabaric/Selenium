f33ed830b8a25604f62dac46de964f3204b5aea5
verabaric <verabaric98@gmail.com>
2022-11-09 17:03:17 +0100

    Udated test cases

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -39,45 +40,66 @@ public class MainTest {
         username.sendKeys("standard_user");
         password.sendKeys("secret_sauce");
         login.click();
+        waitDriver.until(ExpectedConditions.visibilityOfElementLocated(By.id("page_wrapper")));
+        System.out.println("Login was successful");
     }
     @Test(priority = 2)
     public void VerifyHeader(){
-        boolean isHeaderDisplayed = driver.findElement(By.className("title")).isDisplayed();
-        Assert.assertTrue(isHeaderDisplayed,"PRODUCTS header is displayed");
+        waitDriver.until(ExpectedConditions.visibilityOfElementLocated(By.id("header_container")));
+        String actualHeader = driver.findElement(By.className("title")).getText();
+        Assert.assertEquals(actualHeader, "PRODUCTS");
+        System.out.println("PRODUCTS title is displayed");
+       // boolean isHeaderDisplayed = driver.findElement(By.className("title")).isDisplayed();
+        //Assert.assertTrue(isHeaderDisplayed,"PRODUCTS header is displayed");
     }
 
     @Test(priority = 3)
     public void VerifyShoppingCart(){
         boolean isShopppingCartDisplayed = driver.findElement(By.id("shopping_cart_container")).isDisplayed();
         Assert.assertTrue(isShopppingCartDisplayed,"Shopping Cart is displayed");
+        System.out.println("Shopping Cart is displayed");
     }
 
     @Test(priority = 4)
     public void VerifyBurgerMenu(){
         boolean isBurgerMenuDisplayed = driver.findElement(By.id("menu_button_container")).isDisplayed();
         Assert.assertTrue(isBurgerMenuDisplayed,"Menu is displayed");
+        System.out.println("Menu is displayed");
     }
 
     @Test(priority = 5)
     public void VerifyFacebookLink(){
         waitDriver.until(ExpectedConditions.visibilityOfElementLocated(By.className("social_facebook")));
+        boolean isFacebookEnabled = driver.findElement(By.xpath("//*[@id=\"page_wrapper\"]/footer/ul/li[2]/a")).isEnabled();
+        Assert.assertTrue(isFacebookEnabled,"Facebook is enabled");
+        System.out.println("Facebook button is enabled");
     }
 
     @Test(priority = 6)
     public void VerifyTwitterLink(){
         waitDriver.until(ExpectedConditions.visibilityOfElementLocated(By.className("social_twitter")));
+        boolean isFTwitterEnabled = driver.findElement(By.xpath("//*[@id=\"page_wrapper\"]/footer/ul/li[1]/a")).isEnabled();
+        Assert.assertTrue(isFTwitterEnabled,"Twitter is enabled");
+        System.out.println("Twitter button is enabled");
     }
 
     @Test(priority = 7)
     public void VerifyLinkedInLink(){
         waitDriver.until(ExpectedConditions.visibilityOfElementLocated(By.className("social_linkedin")));
+        boolean isLinkedInEnabled = driver.findElement(By.xpath("//*[@id=\"page_wrapper\"]/footer/ul/li[3]/a")).isEnabled();
+        Assert.assertTrue(isLinkedInEnabled,"LinkedIn is enabled");
+        System.out.println("LinkedIn button is enabled");
     }
 
     @Test(priority = 8)
     public void Logout(){
         WebElement meni = driver.findElement(By.className("bm-burger-button"));
         meni.click();
+        waitDriver.until(ExpectedConditions.visibilityOfElementLocated(By.className("bm-menu-wrap")));
+        System.out.println("Menu displayed");
+        waitDriver.until(ExpectedConditions.visibilityOfElementLocated(By.id("logout_sidebar_link")));
         WebElement logout=driver.findElement(By.id("logout_sidebar_link"));
         logout.click();
+        System.out.println("User logged out");
     }
 }
--- src/test/java/org/example/TestCase2.java
+++ src/test/java/org/example/TestCase2.java
@@ -19,7 +19,7 @@ public class TestCase2 {
     @BeforeClass
     public void setUp() {
         driver = new ChromeDriver();
-        waitDriver = new WebDriverWait(driver, Duration.ofSeconds(5));
+        waitDriver = new WebDriverWait(driver, Duration.ofSeconds(6));
         driver.manage().window().maximize();
         driver.get("https://www.saucedemo.com/");
 
@@ -38,48 +38,60 @@ public class TestCase2 {
         username.sendKeys("standard_user");
         password.sendKeys("secret_sauce");
         login.click();
+        waitDriver.until(ExpectedConditions.visibilityOfElementLocated(By.id("inventory_container")));
+        System.out.println("Login successfully");
     }
 
     @Test(priority = 2)
     public void SLBitemSelected(){
         WebElement ranac = driver.findElement(By.id("item_4_title_link"));
         ranac.click();
+        waitDriver.until(ExpectedConditions.visibilityOfElementLocated(By.className("inventory_item_container")));
+        System.out.println("Sauce Labs Backpack Selected");
     }
 
     @Test(priority = 3)
     public void VerifySLBdetails(){
         waitDriver.until(ExpectedConditions.visibilityOfElementLocated(By.className("inventory_details_desc_container")));
+        boolean isDetailsDisplayed = driver.findElement(By.className("inventory_details_desc_container")).isDisplayed();
+        Assert.assertTrue(isDetailsDisplayed,"Title, description and price are displayed");
+        System.out.println("Title, description and price are displayed");
         WebElement add = driver.findElement(By.id("add-to-cart-sauce-labs-backpack"));
         add.click();
+        System.out.println("Sauce Labs Backpack added to cart");
     }
 
     @Test(priority = 4)
     public void BackButton(){
-        waitDriver.until(ExpectedConditions.visibilityOfElementLocated(By.className("inventory_details_desc_container")));
         WebElement goBack = driver.findElement(By.id("back-to-products"));
         goBack.click();
+        waitDriver.until(ExpectedConditions.visibilityOfElementLocated(By.id("inventory_container")));
+        System.out.println("Back button navigated user to initial webpage successfully");
     }
 
     @Test(priority = 5)
     public void SLBLaddItem(){
         WebElement addSLBL = driver.findElement(By.id("add-to-cart-sauce-labs-bike-light"));
         addSLBL.click();
+        System.out.println("Sauce Labs Bike Light added to cart");
     }
     @Test(priority = 6)
     public void ShoppingCart(){
         waitDriver.until(ExpectedConditions.visibilityOfElementLocated(By.className("shopping_cart_container")));
         WebElement goToCart = driver.findElement(By.className("shopping_cart_link"));
         goToCart.click();
+        waitDriver.until(ExpectedConditions.visibilityOfElementLocated(By.className("cart_contents_container")));
+        System.out.println("Shopping Cart opened successfully");
     }
     @Test(priority = 7)
     public void Checkout(){
-        waitDriver.until(ExpectedConditions.visibilityOfElementLocated(By.className("cart_contents_container")));
         WebElement checkout = driver.findElement(By.id("checkout"));
         checkout.click();
+        waitDriver.until(ExpectedConditions.visibilityOfElementLocated(By.className("checkout_info_container")));
+        System.out.println("User navigated to checkout page successfully");
     }
     @Test(priority = 8)
     public void OrderDetails(){
-        waitDriver.until(ExpectedConditions.visibilityOfElementLocated(By.className("checkout_info_container")));
         WebElement firstname=driver.findElement(By.id("first-name"));
         WebElement lastname=driver.findElement(By.id("last-name"));
         WebElement zipcode=driver.findElement(By.id("postal-code"));
@@ -88,24 +100,31 @@ public class TestCase2 {
         lastname.sendKeys("Baric");
         zipcode.sendKeys("37000");
         continuebutton.click();
+        waitDriver.until(ExpectedConditions.visibilityOfElementLocated(By.className("checkout_summary_container")));
+        System.out.println("User information filled out successfully");
     }
     @Test(priority = 9)
     public void Finish(){
-        waitDriver.until(ExpectedConditions.visibilityOfElementLocated(By.className("checkout_summary_container")));
         WebElement finish = driver.findElement(By.id("finish"));
         finish.click();
+        waitDriver.until(ExpectedConditions.visibilityOfElementLocated(By.className("checkout_complete_container")));
+        System.out.println("User navigated to checkout: complete page successfully");
     }
     @Test(priority = 10)
     public void ThankYouMessage(){
-        waitDriver.until(ExpectedConditions.visibilityOfElementLocated(By.className("checkout_complete_container")));
         boolean isMessageDisplayed = driver.findElement(By.className("complete-header")).isDisplayed();
         Assert.assertTrue(isMessageDisplayed,"Thank you message is displayed");
+        System.out.println("Thank you message is displayed");
     }
     @Test(priority = 11)
     public void Logout(){
         WebElement meni = driver.findElement(By.className("bm-burger-button"));
         meni.click();
+        waitDriver.until(ExpectedConditions.visibilityOfElementLocated(By.className("bm-menu-wrap")));
+        System.out.println("Menu displayed");
+        waitDriver.until(ExpectedConditions.visibilityOfElementLocated(By.id("logout_sidebar_link")));
         WebElement logout=driver.findElement(By.id("logout_sidebar_link"));
         logout.click();
+        System.out.println("User logged out");
     }
 }
